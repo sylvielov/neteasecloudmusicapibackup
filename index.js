c1861bccbdfdb588978f8a924b5e8bdf22789c4e
Sylvielov <ganzihan20072015@163.com>
2026-05-19 17:24:04 +0800

    fix: adapt for Vercel serverless deployment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-require('./app.js')
+const { getModulesDefinitions, consturctServer } = require('./server')
+
+let app = null
+
+module.exports = async (req, res) => {
+  if (!app) {
+    const moduleDefs = await getModulesDefinitions()
+    app = await consturctServer(moduleDefs)
+  }
+  return app(req, res)
+}
