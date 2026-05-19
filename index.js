eb5a63b799eadb32eebcf1de2769eb82ffcbb48c
Sylvielov <ganzihan20072015@163.com>
2026-05-19 18:12:16 +0800

    fix: create anonymous_token file before requiring modules

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const fs = require('fs')
+const path = require('path')
+const tmpPath = require('os').tmpdir()
+
+if (!fs.existsSync(path.resolve(tmpPath, 'anonymous_token'))) {
+  fs.writeFileSync(path.resolve(tmpPath, 'anonymous_token'), '', 'utf-8')
+}
+
 const { consturctServer } = require('./server')
 const { generateRandomChineseIP } = require('./util/index')
 
